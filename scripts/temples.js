5a3f5dde6c501a9827789dd718962224c32b285e
Sitsostyle <71554005+Sitsostyle@users.noreply.github.com>
2026-09-16 22:08:02 -0700

    add changes to temples.html and add new images, scripts, and styles for temples page

---
+++ scripts/temples.js
@@ -0,0 +1,14 @@
+// Populate copyright year
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// Populate last modified date and time
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+// Hamburger menu toggle logic
+const mainNav = document.querySelector('.navigation');
+const hamburgerButton = document.querySelector('#menu');
+
+hamburgerButton.addEventListener('click', () => {
+    mainNav.classList.toggle('open');
+    hamburgerButton.classList.toggle('open');
+});
